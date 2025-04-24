e992138712e8f33bcb3add6ee2975c83d23a731a
I S L A M <litwatch011@gmail.com>
2025-04-24 02:34:38 +0200

    Create Onboarding View & Onboarding View Body

---
+++ lib/features/onboarding/presentation/view/onboarding_view.dart
@@ -0,0 +1,11 @@
+import 'package:clothique/features/onboarding/presentation/view/widgets/onboarding_view_body.dart';
+import 'package:flutter/material.dart';
+
+class OnboardingView extends StatelessWidget {
+  const OnboardingView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: OnboardingViewBody(),);
+  }
+}
